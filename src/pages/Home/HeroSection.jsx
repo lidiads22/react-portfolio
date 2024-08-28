a59648410603b460f1a54df60bc5a39e9b57df15
lidiads22 <Lidiads22@icloud.com>
2024-08-27 23:33:57 -0500

    Hero section complete

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,4 +1,25 @@
 export default function HeroSection() {
-    return <h1>HeroSection</h1>;
-    
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Lidia</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Aspiring</span>{" "}
+            <br />
+            Frontend Developer
+          </h1>
+          <p className="hero--section-description">
+            I'm a Computer Science major at UTSA with a passion for crafting beautiful and responsive user interfaces. <br />
+            Eager to turn complex problems into simple, elegant solutions through code.
+          </p>
+        </div>
+        <button className="btn btn-primary">Let's Connect</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="" alt="Hero Section" />
+      </div>
+    </section>
+  );
 }
+
